a2fb17ebd7d7a8bd6025e7477bafa6fc924c3d23
shashankappu21 <93812288+shashankappu21@users.noreply.github.com>
2023-07-16 23:06:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,68 +2,79 @@ var cup1 = document.getElementById('cup1');
 var cup2 = document.getElementById('cup2');
 var cup3 = document.getElementById('cup3');
 var ball = document.getElementById('ball');
-var positionQueue = [];
-var targetCup = cup1; // Assume initial target cup is cup1
+const detailsOfCups = [cup1,cup2,cup3];
 
-function moveCupToPosition(cup, x, y) {
-  // Add the target position to the queue
-  positionQueue.push({ cup: cup, x: x, y: y });
+function moveCup(cup, x){
+  cup.style.left = x+'px';
+}
 
-  // If this is the only position in the queue, start the animation
-  if (positionQueue.length === 1) {
-    animateNextPosition();
+function shuffleArray(array) {
+  const n = array.length;
+  for (let i = n - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
+  return array;
+}
 
-  function animateNextPosition() {
-    if (positionQueue.length > 0) {
-      var nextPosition = positionQueue[0];
-
-      // Move the cup to the next position
-      nextPosition.cup.style.top = nextPosition.y + 'px';
-      nextPosition.cup.style.left = nextPosition.x + 'px';
-
-      // Wait for the animation to finish
-      setTimeout(function () {
-        // Remove the processed position from the queue
-        positionQueue.shift();
-
-        // Continue to the next position in the queue
-        animateNextPosition();
-      }, 1000); // Adjust the time delay (in milliseconds) to match the transition duration
+function getPosition(array){
+  for(let i=0;i<3;i++){
+    if(array[i] === cup1){
+      return i;
     }
   }
 }
 
+
+function rotateCups(){
+  setTimeout(function(){
+    const shuffledDetails = shuffleArray(detailsOfCups);
+  // console.log(shuffledDetails);
+  moveCup(shuffledDetails[0],400);
+  moveCup(shuffledDetails[1],600);
+  moveCup(shuffledDetails[2],800);
+  let x = getPosition(shuffledDetails);
+  ball.style.left = (422 + x*200)+'px';
+  },2000);
+  // setTimeout(function(){
+  //   for(let i=0;i<3;i++){
+  //     ball.style.display = 'block';
+  //     detailsOfCups[i].style.opacity = 0.5;
+  //   }
+  // },3000);
+}
+
 function handleCupClick(cup) {
-  if (cup === targetCup) {
+  if (cup === cup1) {
     alert('Congratulations! You found the ball!');
   } else {
     alert('Sorry! Try again.');
   }
 }
 
-document.getElementById('startButton').addEventListener('click', function () {
-  // Hide the ball
-  ball.style.display = 'none';
-
-  // Change the cups' z-index and interchange their positions
-  cup1.style.zIndex = 2;
-  cup2.style.zIndex = 3;
-  cup3.style.zIndex = 1;
-
-  moveCupToPosition(cup1, 600, 250);
-  moveCupToPosition(cup2, 800, 250);
-  moveCupToPosition(cup3, 400, 250);
-});
-
-cup1.addEventListener('click', function () {
-  handleCupClick(cup1);
-});
-
-cup2.addEventListener('click', function () {
-  handleCupClick(cup2);
-});
-
-cup3.addEventListener('click', function () {
-  handleCupClick(cup3);
-});
+function playGame(){
+  setTimeout(function(){
+    for(let i=0;i<3;i++){
+      detailsOfCups[i].style.opacity = 0.5;
+    }
+  },500);
+  setTimeout(function(){
+    for(let i=0;i<3;i++){
+      detailsOfCups[i].style.opacity = 1;
+    }
+    ball.style.display = 'none';
+  },1500);
+  
+  rotateCups();
+  cup1.addEventListener('click', function () {
+    handleCupClick(cup1);
+  });
+  
+  cup2.addEventListener('click', function () {
+    handleCupClick(cup2);
+  });
+  
+  cup3.addEventListener('click', function () {
+    handleCupClick(cup3);
+  });
+}
